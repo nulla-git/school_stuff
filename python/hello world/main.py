dd41de68b3210a43164e2e34a816e55874d60846
nulla-git <blakereneau0309@gmail.com>
2022-08-18 12:11:39 -0500

    capybaras, not capybara

--- python/hello world/main.py
+++ python/hello world/main.py
@@ -1,4 +1,4 @@
 print("Hello Blake")
 print("Corn was indredibly small before domestication.")
-print("Capybara are cool!")
+print("Capybaras are cool!")
 print("I wonder when i'll be forgot to time.")
