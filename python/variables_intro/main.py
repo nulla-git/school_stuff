7fe2f6246d905f8acd1523eefb441899e70833f5
nulla-git <blakereneau0309@gmail.com>
2022-08-19 12:37:10 -0500

    added variable stuff

---
+++ python/variables_intro/main.py
@@ -0,0 +1,23 @@
+x = 3
+y = 2
+bar = 5
+jimmy = "john"
+john = "jimmy "
+
+print(x + y);
+print(john + jimmy)
+
+# /* i swear if there's any more syntax sugar
+# i'm gonna die from diabetes */
+# why doesn't python do anything consistent
+# i swear if i can't do some more advanced
+# i'm gonna have to put here be dragons everywhere
+
+def foo():
+    bar = 7
+    print(x + bar);
+
+foo()
+
+
+print(x + bar)
