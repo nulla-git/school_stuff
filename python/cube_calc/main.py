a1cd488bcb73ada76eb99b546cbdf71dbde7e122
nulla-git <blakereneau0309@gmail.com>
2022-09-01 12:35:39 -0500

    finished cube_calc

--- python/cube_calc/main.py
+++ python/cube_calc/main.py
@@ -1,13 +1,14 @@
 #Blake Reneau 09/01/2022
 #a simple volume and surface area calculator for cubes
 
-length_tb = input.int("what is top or bottom length? ")
-width_tb = input.int("what is top or bottom width? ")
-height_fb = input.int("what is front or back height? ")
-width_fb = input.int("what is front or back width? ")
-hight_lr = input.int("what is front or back height? ")
-width_lr = input.int("what is front or back width? ")
+length_tb = int(input("what is top or bottom length? "))
+width_tb = int(input("what is top or bottom width? "))
+height_fb = int(input("what is front or back height? "))
+width_fb = int(input("what is front or back width? "))
+hight_lr = int(input("what is front or back height? "))
+width_lr = int(input("what is front or back width? "))
 
+surface_area = (((length_tb * width_tb) * 2) + ((width_fb * height_fb) * 2) + ((width_fb * height_fb) * 2))
 
-
-print("volume is:",width_tb * 
+print("volume is:",width_tb * length_tb * height_fb) 
+print("surface area is:",surface_area)
