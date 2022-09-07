f3381cab2f77620072bb787a5ed8b6573b517191
nulla-git <blakereneau0309@gmail.com>
2022-09-07 11:39:00 -0500

    further updated if else assignment

--- python/if_else_assignment/main.py
+++ python/if_else_assignment/main.py
@@ -2,6 +2,7 @@ number = 5
 print("I have thought of a number between 1 and 10")
 guess = int(input("Can you guess what it is? "))
 
+#I'm not using else here because indentation is not a place of honor.
 if guess < number:
      print("Never mind")
 if guess > number:
