4c85220bae3cbf79a1ef33af74d7edc523d43733
nulla-git <blakereneau0309@gmail.com>
2022-09-09 12:07:24 -0500

    updated grader

--- python/grader/main.py
+++ python/grader/main.py
@@ -1,15 +1,25 @@
 #Blake Reneau 9/8/22
 #a simple program to automatically grade a score.
+import sys
 
 score = int(input("what's the score? "))
 
-if 40 > score:
+#this logic should work but it doesn't.
+#it's commented out for now, may look at it later. 
+
+# if score > 100 or < 0:
+#    print("invalid!")
+#    sys.exit()
+
+#elif simply means else if a condition is met.
+
+if score < 40:
     print("Grade is U.")
-if 40 <= score >= 59:
+elif 40 <= score <= 59:
     print("Grade is C.")
-if 69 <= score >=79:
+elif 60 <= score <= 79:
     print("Grade is B.")
-if 80 <= score:
+elif 80 <= score:
     print("Grade is A.")
 
 #notice that else here has a colon immediately in front of it,
