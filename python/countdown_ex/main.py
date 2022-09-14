3f97bcf898c6d32dae45b1ca00ec79834977f589
nulla-git <blakereneau0309@gmail.com>
2022-09-14 11:16:04 -0500

    added square calc

---
+++ python/countdown_ex/main.py
@@ -0,0 +1,12 @@
+#Blake Reneau 09/13/22
+#simple countdown example
+
+i = 42
+
+while i > 20:
+    i = i-3
+    if i == 18:
+        break
+    print(i)
+
+
---
+++ python/squares/main.py
@@ -0,0 +1,12 @@
+#Blake Reneau 9/13/22
+#simple program that prints squares of numbers.
+
+i = 1
+n = 1
+
+print("Number\tSquare")
+
+while i < 11:
+    print(n, "\t" , n * n)
+    i += 1
+    n += 1
