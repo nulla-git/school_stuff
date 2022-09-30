93cfe97bcef94dde4b8ef07a47682fee44d09bc9
nulla-git <blakereneau0309@gmail.com>
2022-09-30 09:26:45 -0500

    airport simulator works now

--- python/airport_operator/main.py
+++ python/airport_operator/main.py
@@ -1,6 +1,7 @@
 #Blake Reneau 9/29/22
 #simple airport operator simulator
 
+#maximum requests are 5
 requests = 0
 #maximum capacity needs to be 3
 capacity = 0
@@ -8,6 +9,9 @@ capacity = 0
 print("luton airport is open")
 
 while requests < 5:
+    if capacity == 3:
+        print("this is working right")
+    print("currenct capacity:",capacity)
     requests += 1
     print("please state condition and intentions.")
     weather = input("what is the weather? (sunny, cloudy, or rainy): ")
@@ -16,14 +20,14 @@ while requests < 5:
     takeoff_allowed = ((weather == "sunny" and windSpeed <= 30) or
         (weather == "cloudy" and windSpeed <= 20) or 
         (weather == "rainy" and windSpeed <= 10))
-    landing_allowed = ((weather == "sunny" and windSpeed <= 25) or
-        (weather == "cloudy" and windSpeed <= 15) or 
-        (weather == "rainy" and windSpeed <= 5))
+    landing_allowed = ((weather == "sunny" and windSpeed <= 25 and capacity < 3) or
+        (weather == "cloudy" and windSpeed <= 15 and capacity < 3) or 
+        (weather == "rainy" and windSpeed <= 5 and capacity < 3))
     if intent == "land" and landing_allowed == True:
-        capacity += 1
+        capacity = capacity + 1
         print("free to land.")
         continue
-    elif landing_allowed == False or capacity == 3:
+    elif landing_allowed == False:
         print("the conditions here are unsafe to land, find somewhere else.")
         continue
     if intent == "takeoff" and takeoff_allowed == True:
