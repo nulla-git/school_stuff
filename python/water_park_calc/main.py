e86be2c546cf2ed4fc3614724b7e535bfceb726c
nulla-git <blakereneau0309@gmail.com>
2022-10-06 11:24:58 -0500

    added water park sim

---
+++ python/water_park_calc/main.py
@@ -0,0 +1,22 @@
+#Blake Reneau 10/5/2022
+#total price calculator for a fake water park.
+print("Entrance Ticket Price Chart:")
+print("Adult: $15")
+print("Child: $10 (Younger than 18)")
+print("If your entry tickets are higher than or equal to 50,\nthen you are automatically granted a 5% discout.")
+
+adults = int(input("how many adults? "))
+children = int(input("how many children? "))
+
+adults *= 15
+children *= 11
+total = (adults + children)
+
+if total > 50:
+    total = total * .95
+    round(total, 2)
+    print("your total with a discout is $" + str(total))
+elif total <= 50:
+    print("your total is $" + str(total))
+
+print("have a great day!")
