e3b75d7fa4ea94dfe495ab98f94f9373ff71fdd0
nulla-git <blakereneau0309@gmail.com>
2022-10-13 12:43:10 -0500

    fixed functions example

--- python/functions_subroutines/main.py
+++ python/functions_subroutines/main.py
@@ -1,41 +1,42 @@
 #Blake Reneau 10/13/2022
 #example of how functions/subroutines can be used.
 
-def add(foo):
-    total = foo + 5
+def add(foo,bar):
+    total = foo + bar
     print(total)
 
-def sub(foo):
-    total = foo - 5
+def sub(foo,bar):
+    total = foo - bar
     print(total)
 
-def mult(foo):
-    total = foo * 5
+def mult(foo,bar):
+    total = foo * bar
     print(total)
 
-def div(foo):
-    total = foo / 5
+def div(foo,bar):
+    total = foo / bar
     print(total)
 
-choice = int(input("Input 1 to add a number to 5, 2 to subtract 5 from, 3 to multiply by five, or 4  to divide five (use any other number to quit): "))
+choice = int(input("Input 1 to add two numbers, 2 to subtract 2 numbers, 3 to multiply by two numbers, or 4 to divide two numbers (use any other number to quit): "))
 i = 0
 
 while 5 > choice > 0:
     i += 1
     if i > 1:
-        choice = int(input("Input 1 to add a number to 5, 2 to subtract 5 from, 3 to multiply by five, or 4  to divide five (use any other number to quit): "))
-    number = int(input("number to use? "))
+        choice = int(input("Input 1 to add two numbers, 2 to subtract 2 numbers, 3 to multiply by two numbers, or 4 to divide two numbers (use any other number to quit): "))
+    number1 = int(input("1st number to use: "))
+    number2 = int(input("2nd number to use: "))
     if choice == 1:
-        add(number)
+        add(number1,number2)
         continue
     elif choice == 2:
-        sub(number)
+        sub(number1,number2)
         continue
     elif choice == 3:
-         mult(number)
+         mult(number1,number2)
          continue
     elif choice == 4:
-        div(number)
+        div(number1,number2)
         continue
     else: 5 > choice > 0
     break
