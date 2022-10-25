fc20cd83bb21a9ecf2d9b91b29fa25754ee5d9e6
nulla-git <blakereneau0309@gmail.com>
2022-10-25 12:28:40 -0500

    updated classes example

--- python/my_first_class/main.py
+++ python/my_first_class/main.py
@@ -10,8 +10,10 @@ class dog:
 
 jacob = dog("jacob", "Corgi", 5, "Black")
 molly = dog("molly", "pomeranian", 7, "white")
+mark = dog("mark", "golden retriever", 4, "golden")
+bob = dog("bob", "chihuawa", 2, "brown")
 
 print(jacob.name + " is a " + jacob.color + ", " + str(jacob.age) + " year old " + jacob.breed)
 print(molly.name + " is a " + molly.color + ", " + str(molly.age) + " year old " + molly.breed)
-
-
+print(mark.name + " is a " + mark.color + ", " + str(mark.age) + " year old " + mark.breed)
+print(bob.name + " is a " + bob.color + ", " + str(bob.age) + " year old " + bob.breed)
