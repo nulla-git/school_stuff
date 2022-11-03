6cc8a6edf34da378c1e75db57bbc2a5da86c0d0c
nulla-git <blakereneau0309@gmail.com>
2022-11-03 12:32:35 -0500

    updated file intro

--- python/files_intro/main.py
+++ python/files_intro/main.py
@@ -1,12 +1,31 @@
-#blake reneau 2/11/22
+#blake reneau 2/11/2
 #file printing example
+import random
 
 foo = open("drinks.txt", "r")
-#print(foo.read()) 
+bar = open("poisons.txt", "r")
+yum = []
+yuck = []
+yum = foo.read().splitlines()
+yuck = bar.read().splitlines()
+yum_rand = random.choices(yum)
+yuck_rand = random.choices(yuck)
+print("random item from yum: " + str(yum_rand))
+print("random item from yuck: " + str(yuck_rand))
 
 
-i = 0
-while i < 11:
-    print(foo.readline())
-    i += 1
+"""
+print("here's some drinks that i like: ")
+print(foo.read()) 
+print("and here's just some comestibles in general that i don't like: ")
+print(bar.read())
+"""
 
+"""
+" proof of concept teacher made me do
+"
+"i = 0
+"while i < 11:
+"    print(foo.readline())
+"    i += 1
+"""
