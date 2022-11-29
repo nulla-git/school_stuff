94bad344353450514c3c114040f9442577a81693
nulla-git <blakereneau0309@gmail.com>
2022-11-29 12:03:13 -0600

    added min-max sorter

---
+++ python/min_max_sorter/main.py
@@ -0,0 +1,15 @@
+# 11/29/22 Blake Reneau
+# Ask the user for three numbers and return the smallest and largest value.
+
+num1 = int(input("I need a number: "))
+num2 = int(input("and another... : "))
+num3 = int(input("and another... : "))
+
+numbers = []
+numbers.insert(0, num1)
+numbers.insert(0, num2)
+numbers.insert(0, num3)
+numbers.sort()
+
+print("the smallest number is " + str(numbers[0]) )
+print("the largest number is " + str(numbers[2]) )
--- python/rock_paper_scissors_fixed/test.py
+++
@@ -1,7 +0,0 @@
-foo = 1
-i = 0
-
-if foo == 2:
-    print("foo is 2")
-else: foo == 1
-print("foo is 1")
