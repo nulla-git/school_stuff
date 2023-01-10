046316ddcd07b57e4d31ca20dc61685afe795937
nulla-git <blakereneau0309@gmail.com>
2023-01-10 12:37:32 -0600

    added turtle expirements

---
+++ python/DYWYPI/main.py
@@ -0,0 +1,62 @@
+#Blake Reneau 01/09/23
+#drawing a common phrase from nethack.
+import turtle
+from turtle import *
+
+turt1 = turtle.Turtle()
+turt2 = turtle.Turtle()
+turt3 = turtle.Turtle()
+turt4 = turtle.Turtle()
+turt5 = turtle.Turtle()
+
+# letter D
+turt1.right(90)
+turt1.forward(200)
+turt1.left(90)
+turt1.circle(100, 180)
+
+# letter o, u, a
+#turt2.speed(1)
+turt2.penup()
+turt2.forward(200)
+turt2.right(90)
+turt2.forward(100)
+turt2.pendown()
+turt2.circle(100)
+turt2.penup()
+turt2.forward(100)
+turt2.left(90)
+turt2.forward(600)
+turt2.pendown()
+turt2.circle(100)
+turt2.penup()
+turt2.left(90)
+turt2.forward(100)
+turt2.right(90)
+turt2.forward(200)
+turt2.right(90)
+turt2.pendown()
+turt2.circle(100, 180)
+
+# letter y
+turt3.penup()
+turt3.right(90)
+turt3.forward(100)
+turt3.left(90)
+turt3.forward(600)
+turt3.left(135)
+turt3.pendown()
+turt3.forward(100)
+turt3.right(180)
+turt3.forward(100)
+turt3.left(90)
+turt3.forward(100)
+turt3.right(180)
+turt3.forward(100)
+turt3.left(45)
+turt3.forward(100)
+
+# letter w
+turt4.penup()
+turt4.forward(1200)
+input()
---
+++ python_test/turtle_test.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+pensize(10)
+speed(1)
+turt1 = turtle.Turtle()
+turt2 = turlte.Turlte()
+
+
+# letter D
+turt1.right(90)
+turt1.forward(200)
+turt1.left(90)
+turt1.circle(100, 180)
+
+# letter o
+turt2.penup()
+turt2.forward(200)
+turt2.right(90)
+turt2.forward(100)
+turt2.circle(100)
+input()
