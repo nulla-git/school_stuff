c5e24510c8ee9b6d2a604a5b8aba767a5886f0e0
nulla-git <blakereneau0309@gmail.com>
2023-01-12 12:09:49 -0600

    had to make a star

---
+++ python/draw_star/main.py
@@ -0,0 +1,17 @@
+# Blake Reneau 12/1/23
+# drawing a star with turtle
+import turtle
+import math
+
+john = turtle.Turtle()
+
+john.speed(1)
+john.penup()
+john.setpos(0, 80)
+john.pendown()
+john.setpos(-50, -100)
+john.setpos(100, 0)
+john.setpos(-100,0)
+john.setpos(50, -100)
+john.setpos(0, 80)
+input()
