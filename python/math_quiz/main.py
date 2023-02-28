6c10ff2976248be59f9740ba2484d6f7af436a96
nulla-git <blakereneau0309@gmail.com>
2023-02-28 12:12:55 -0600

    add fractions to math quiz

--- python/math_quiz/main.py
+++ python/math_quiz/main.py
@@ -2,6 +2,7 @@
 # random times table quiz
 import os
 import random
+from fractions import Fraction
 
 # I don't neeeeed this it's just a nicety
 os.system('clear')
@@ -14,19 +15,25 @@ answer = 0
 while i < questions:
     i += 1
     #question type
-    qtype = random.randrange(0, 3)
+    qtype = random.randrange(0, 4)
     num1 = random.randrange(1, 10)
     num2 = random.randrange(1, 10)
 
     if qtype == 0:
         correct = num1 + num2
         answer = int(input("what is " + str(num1) + " plus " + str(num2) + "? "))
-    if qtype == 1 and num1 > num2:
+    if qtype == 1:
         correct = num1 - num2
         answer = float(input("what is " + str(num1) + " minus " + str(num2) + "? "))
     if qtype == 2:
         correct = num1 * num2
         answer = int(input("what is " + str(num1) + " times " + str(num2) + "? "))
+    if qtype == 3:
+        correct = num1 / num2
+        answer = (input("what is " + str(num1) + " divided by " + str(num2) + "? "))
+        #cheat if you want lol
+        answer = Fraction(answer)
+        answer = float(answer)
 
     if answer == correct:
         print("correct!")
