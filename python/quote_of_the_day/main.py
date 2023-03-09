49f872ce1f6ac1e073af06193ef15851bc903e25
nulla-git <blakereneau0309@gmail.com>
2023-03-09 11:15:00 -0600

    function daily quotes now

--- python/quote_of_the_day/main.py
+++ python/quote_of_the_day/main.py
@@ -1,4 +1,4 @@
-#Blake Reneau 3/6/2023
+#Blake Reneau, Gayle Jones 3/6/2023
 #Quote of the day program
 import random
 import os
@@ -7,30 +7,33 @@ os.system('clear')
 question_type = random.randrange(0, 4)
 num1 = random.randrange(1, 100)
 num2 = random.randrange(1, 100)
+quoteList = []
 quotes = open("quotes.txt", "r")
 with quotes as fp:
     #value of variable is equal to the amount of lines, of which each line
     #has a value of one.
     num_lines = sum(1 for line in fp)
 quotes = open("quotes.txt", "r")
+for line in quotes:
+	quoteList.append(line)
 print(num_lines)
 quote_pick = random.randrange(0, num_lines)
-print(quote_pick)
-
-print(quotes.readlines()[8])
+print("quote pick #", quote_pick)
 
 print("####################")
 print("# Quote of the day #")
 print("####################")
-print(quotes.readlines()[quote_pick])
+#print(quotes.readlines()[quote_pick])
+print(random.choice(quoteList))
 quotes.close
 
 print("###########################")
 print("# Spanish word of the day #")
 print("###########################")
-words = open("spanish_words.txt", "r")
-print(words.readlines()[quote_pick])
-words.close
+#words = open("spanish_words.txt", "r")
+words = ["aqui - here", "dia - day", "nino - son", "nosotros - we", "tu - i"]
+print(random.choice(words))
+
 
 print("########################")
 print("# Daily Math Challenge #")
@@ -53,7 +56,3 @@ quote_append = open("quotes.txt", "a")
 quote_append.write("\n")
 quote_append.write(append_text)
 quote_append.close
-
-
-
-
