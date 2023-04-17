56f343893b4ef5ad1b4a0969466c21494707248b
nulla-git <blakereneau0309@gmail.com>
2023-04-17 11:04:20 -0500

    back to the future thing

---
+++ python/date_day_difference/main.py
@@ -0,0 +1,9 @@
+# Blake Reneau 04/14/23
+# day difference from date, duh.
+from datetime import *
+
+name_of_list = date.str.split("/")
+day = int(name_of_list[0])
+month = 1
+year = 2
+#date(year,month,day)
