757896d331328e9bb1bcfe86f88ee09831167f51
nulla-git <blakereneau0309@gmail.com>
2023-04-25 12:30:00 -0500

    menu project

--- python/to_go_sim/main.py
+++ python/to_go_sim/main.py
@@ -15,5 +15,29 @@ for line in rawMenuData:
     itemList.update({menuItem: price})
 
 print("welcome to Peppino's Pizzeria")                      
-print("yes we do get regularly sued for false advertising")
+print("no, we do not serve pizza.\nyes, we do get regularly sued for false advertising")
+total = float(0)
+request = input("enter the code for the item you want: ")
+finished = False
+firstRun = True
+# ok this is a little complicated so i'll go ahead and explain.
+# 
+while finished == False:
+    if firstRun == False:
+        request = input("what else do you want (enter code)? ") 
+    itemPosition = codeList.index(request)
+    itemListKeys = list(itemList.keys())
+    keyNameOfItem = ((list(itemListKeys))[itemPosition])
+    costOfItem = itemList[keyNameOfItem]
+    print(str(keyNameOfItem) + ": $" + str(costOfItem))
+    total += float(costOfItem)
+    print("new total: $" + str(round(total, 2)))
+    wantAnother = input("do you want to order anything else? (Y/N) ")
+    if wantAnother == "Y" or wantAnother == "y":
+        firstRun = False 
+    if wantAnother == "N" or wantAnother =="n":
+        finished = True
 
+
+print("Your toal comes out to $" + str(round(total, 2)))
+print("thank you for not suing us when we told you our name is a lie")
